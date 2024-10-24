5233cfad5ca4ab720bf48340590e0fe52e150f77
hypnosis-games <>
2024-10-24 13:09:13 -0400

    Update you-want-to-be-hypnotized.js

--- app/scripts/you-want-to-be-hypnotized.js
+++ app/scripts/you-want-to-be-hypnotized.js
@@ -8,17 +8,29 @@ Maybe the warm... soft... relaxing... intoxicating feeling of slipping into tran
 
 And you find yourself wanting to slip and sink and drift and fall... again and again... over and over... deeper and deeper.
 
-Or maybe it's not so easy... not so familiar... but you love the idea... the idea of hypnotic trance as a soft... warm... relaxing... intoxicating... space that your mind can slip into.
+Or maybe it's not so easy... not so familiar... but you love the idea.
 
-But either way... you're here because you want to be hypnotized. I want you to say it for me. Just say it out loud: "I'm here because I want to be hypnotized."
+You love the idea of hypnotic trance as a soft... warm... relaxing... intoxicating... space that your mind can slip into.
 
-Maybe that feels a little bit silly. We both know I can't hear you... you're just talking to yourself. But it feels good to say out loud... doesn't it? Say it again: "I'm here because I want to be hypnotized." Just let the words out however they come out. 
+But either way... you're here because you want to be hypnotized. I want you to say it for me. 
+
+Just say it out loud: "I'm here because I want to be hypnotized."
+
+Maybe that feels a little bit silly. We both know I can't hear you... you're just talking to yourself. 
+
+But it feels good to say out loud... doesn't it? Say it again: "I'm here because I want to be hypnotized." 
+
+Just let the words out however they come out. 
 
 Maybe it's confident. Maybe it's quiet. Maybe it's a sigh... relaxing into the feeling... accepting and affirming what you want.
 
 And what you want is to be hypnotized... isn't it? 
 
-You have your reasons but right now they don't really matter all that much... because right now... the only thing that really matters is that you want to be hypnotized... and that it feels so nice... so easy... so freeing... to say that desire out loud for me. 
+You have your reasons but right now they don't really matter all that much...
+
+In fact, the only thing that really matters is that you want to be hypnotized... and that it feels so nice... 
+
+If feels so nice... so easy... so freeing... to say that desire out loud for me. 
 
 And you can just say it again for me: "I want to be hypnotized"
 
@@ -26,35 +38,63 @@ knowing that it's easy to say... that it feels good... that it feels right and n
 
 You want to be hypnotized. 
 
-And the lovely thing about trance is that it's so deliciously fuzzy. Maybe you know exactly what trance feels like for you. Maybe you've never felt it before. It doesn't matter... because no one really knows precisely what hypnosis, what trance is... because it isn't actually any singular particular thing. But that's OK... isn't it? 
+And the lovely thing about trance is that it's so deliciously fuzzy. Maybe you know exactly what trance feels like for you. 
 
-That fuzziness... those soft blurry boundaries... they just make you want it more... make you want to be hypnotized more... don't they?
+Maybe you've never felt it before. It doesn't matter... because no one really knows precisely what hypnosis, what trance is... 
+
+The truth is that trance isn't actually any singular particular thing... and whatever you find yourself feeling as your read these words... 
+
+That can be trance for you right now.
 
 You want to be hypnotized. 
 
 And each time you read those words... if you'd like... you can just find yourself repeating them out loud for me... 
 
-Perhaps quietly under your breath... or maybe just mouthing the words and imagining them. Whatever feels right for you is perfect.. You want to be hypnotized: "I want to be hypnotized." You want to be hypnotized: "I want to be hypnotized."
+Perhaps quietly under your breath... or maybe just mouthing the words and imagining them. 
+
+Whatever feels right for you is perfect.. 
+
+You want to be hypnotized: "I want to be hypnotized." You want to be hypnotized: "I want to be hypnotized."
 
 And you don't need to repeat them out loud. 
 
 This isn't a hypnotic command or trigger. It's just a nice suggestion that you might find easy to follow. 
 
-Because the more you say the words... the easier and easier it is just to keep repeating them. You want to be hypnotized. And the more you say the words the harder and harder it is to really think about very much else. 
+Because the more you say the words... the easier and easier it is just to keep repeating them. 
 
-You want to be hypnotized. And it feels so nice to say them... to sort of slip into repeating my words back to me... to acknowledge and affirm and enjoy your own desire to sink into trance. You want to be hypnotized.
+You want to be hypnotized. And the more you say the words the harder and harder it is to really think about very much else. 
 
-And as you focus on that desire you might notice different sensations floating and fluttering through your body. Hypnosis is just a form of communication... of course. It's really a conversation... a back-and-forth. You read my words and respond to them.
+You want to be hypnotized. 
+
+And it feels so nice to say those words... to sort of slip into repeating my words back to me... 
+
+If feels nice to acknowledge and affirm and enjoy your own desire to sink into trance. You want to be hypnotized.
+
+And as you focus on that desire you might notice different sensations floating and fluttering through your body. 
+
+That's just your body responding to the idea... the feeling... the desire to be hypnotized.
 
 You want to be hypnotized.
 
-You read my words and respond to them and let them guide and direct your focus because that's what people do in conversation. We focus on each other. We listen. We let our thoughts be guided and shifted and directed by the exchange. 
+Whatever you're feeling right now is just fine. It's just right. It's just perfect.
+
+Hypnosis is just a form of communication... of course. It's really a conversation... a back-and-forth. 
+
+Your body is just responding to my words.  You are just responding to my words.  Because that's what people do in conversation.
+
+You read my words and respond to them.
+
+You want to be hypnotized.
+
+You read my words and respond to them and let them guide and direct your focus because that's what people do in conversation. 
+
+We focus on each other. We listen. We let our thoughts be guided and shifted and directed by the exchange. 
 
 You want to be hypnotized.
 
 And of course you're just reading my words... so I can't know exactly how you're responding. 
 
-In fact... the only really know one thing about how you're feeling right now.
+In fact... I really only know one thing about how you're feeling right now.
 
 You want to be hypnotized. 
 
@@ -62,7 +102,9 @@ But that's enough for me to make some guesses... to guide you... to direct you..
 
 You want to be hypnotized. 
 
-Thinking and saying those words has probably started to feel very easy... effortless... almost automatic. You want to be hypnotized. And the feeling of acknowledging... of expressing... of affirming this desire is really lovely... and you can feel that loveliness fluttering and floating around inside of you.
+Thinking and saying those words has probably started to feel very easy... effortless... almost automatic. You want to be hypnotized. 
+
+And the feeling of acknowledging... of expressing... of affirming this desire can be so lovely... and you can feel that loveliness fluttering and floating around inside of you.
 
 You might feel a sensation on your skin... perhaps a sort of warmth... or a buzzing and tingling.
 
@@ -72,7 +114,9 @@ You might find that as you repeat the words... your own speech is slowing down..
 
 You want to be hypnotized.
 
-You might find that you're reading slowly now... responding slowly... everything in your mind moving in a lovely... slow... foggy way and that it's hard to really think about anything except what you want.
+You might find that you're reading slowly now... responding slowly... everything in your mind moving in a lovely... slow... foggy way 
+
+Might find that it's hard to really think about anything except what you want.
 
 You want to be hypnotized.
 
@@ -94,7 +138,11 @@ The more you focus on that desire the less room there is inside of your mind for
 
 You want to be hypnotized.
 
-And you might start to realize that that desire is so very large. Filling you up and making you feel so good. So soft and fuzzy. Warm and wonderful. Buzzing and tingling and dreamy and foggy. You might start to realize that this desire is everywhere inside you... like a warm... relaxing... intoxicating fog. 
+And you might start to realize that that desire is so very large. Filling you up and making you feel so good. So soft and fuzzy. 
+
+Warm and wonderful. 
+
+Buzzing and tingling and dreamy and foggy. You might start to realize that this desire is everywhere inside you... like a warm... relaxing... intoxicating fog. 
 
 And that you can sink into it for me. 
 
@@ -102,15 +150,23 @@ Now.
 
 You want to be hypnotized.
 
-And maybe you've been sinking into it for a long time now. You probably don't know exactly when you started to sink... started to slip. Probably don't know exactly when the thing that you wanted became the thing that was happening to you. But everything feels just a little bit different right now... doesn't it? 
+And maybe you've been sinking into it for a long time now. You probably don't know exactly when you started to sink... started to slip.
 
-Soft and warm and fuzzy and buzzing... dreamy and distant and relaxing and wonderful. You wanted to be hypnotized... but you don't need to say that anymore. 
+You probably don't know exactly when the thing that you wanted became the thing that was happening to you. 
 
-Now you can say something new. 
+But everything feels just a little bit different right now... doesn't it? 
+
+Soft and warm and fuzzy and buzzing... dreamy and distant and relaxing and wonderful. 
+
+You want to be hypnotized.... or... you wanted to be hypnotized... 
+
+But now... now you can say something new. 
 
 Now you can say "I am hypnotized."
 
-That's right. You are hypnotized. Focused on my words. Relaxing and following and letting them guide you. Feeling the desire... the pleasure... the need to slip and sink and fall into trance for me fill you up. 
+That's right. You are hypnotized. Focused on my words. Relaxing and following and letting them guide you. 
+
+Feeling the desire... the pleasure... the need to slip and sink and fall into trance for me fill you up. 
 
 You are hypnotized. 
 
@@ -124,9 +180,17 @@ You are hypnotized.
 
 There's no room for anything else.
 
-And you can just enjoy this feeling now. You are hypnotized and each time you say it you relax further. You are hypnotized and each time you read the words you sink deeper and deeper... feel better and better. And you love this feeling of sinking... of falling... of losing yourself in this warm... soft... fuzzy... dreamy... foggy... intoxicating bliss.
+And you can just enjoy this feeling now. You are hypnotized and each time you say it you relax further. 
 
-You are hypnotized. Relaxed. Open. You are hypnotized. It feels so good. It makes you feel so good. You are hypnotized. You wanted it so much and now you have it and feels incredible. You are hypnotized. 
+You are hypnotized and each time you read the words you sink deeper and deeper... feel better and better. 
+
+And you love this feeling of sinking... of falling... 
+
+You love this feeling of losing yourself in this warm... soft... fuzzy... dreamy... foggy... intoxicating bliss.
+
+You are hypnotized. Relaxed. Open. You are hypnotized. It feels so good. It makes you feel so good. You are hypnotized. 
+
+You wanted it so much and now you have it and feels incredible. You are hypnotized. 
 
 You are hypnotized and it feels so good that you can't help but feel your body responding...
 
@@ -138,7 +202,11 @@ You are hypnotized. And it's so lovely.  So easy.  So wonderful.
 
 And you can stay here for as long as you like.  You can stay here for as long as you want.  You can stay here for as long as you need.
 
-But when you're ready to wake up... you can just say the words: "I'm ready to wake up."  And you'll find yourself slowly... easily... evenly... at your own pace... waking up for me... coming back out of trance... and back into yourself.  
+But when you're ready to wake up... you can just say the words: "I'm ready to wake up."  
+
+You can just say the words "I'm ready to wake up"
+
+and you'll find yourself slowly... easily... evenly... at your own pace... waking up for me... coming back out of trance... and back into yourself.  
 
 That's right.  
 

2a9f8985e46b2bbbb8fb5c83d0d121a8d2a2aff1
hypnosis-games <>
2024-11-07 12:47:08 -0500

    Update script-dictionary.js

--- app/scripts/script-dictionary.js
+++ app/scripts/script-dictionary.js
@@ -1,5 +1,6 @@
 import youWantToBeHypnotized from './you-want-to-be-hypnotized.js'
 import sayYes from './say-yes.js'
+import nodAlong from './nod-along.js'
 export default {
   'you-want-to-be-hypnotized': youWantToBeHypnotized,
   'say-yes': sayYes,
